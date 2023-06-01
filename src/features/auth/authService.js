d2561658d9e93544a211c8248691d1e0001970c3
namodynamic <nnamdi4u09@gmail.com>
2023-06-01 20:59:16 +0100

    update

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,14 +1,7 @@
 import axios from "axios";
+import { config } from "../../utils/axiosconfig";
 import { base_url } from "../../utils/base_url";
-const getTokenFromLocalStorage = localStorage.getItem("user")
-  ? JSON.parse(localStorage.getItem("user"))
-  : null;
-const config = {
-  headers: {
-    Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
-    Accept: "application/json",
-  },
-};
+
 const login = async (userData) => {
   const response = await axios.post(`${base_url}user/admin-login`, userData);
   if (response.data) {
